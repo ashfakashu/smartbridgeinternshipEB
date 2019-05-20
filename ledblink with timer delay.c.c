1c5abf163485301de1a218ef78f80fa297d769c1
ashfakashu <50774191+ashfakashu@users.noreply.github.com>
2019-05-20 14:38:28 +0530

    Add files via upload

---
+++ ledblink with timer delay.c.c
@@ -0,0 +1,31 @@
+/*
+ * aa.c
+ *
+ * Created: 5/20/2019 2:38:51 PM
+ * Author : ashfakashu
+ */ 
+
+#include <avr/io.h>
+#define  LED PB0
+
+
+int main(void)
+{
+	uint8_t timeroverflowcount=0;
+	DDRB=0XFF;
+	TCNT0=0X00;
+	TCCR0=(1<<CS00)|(1<<CS02);
+	while(1)
+	{
+		while((TIFR & 0X01) == 0);
+		TCNT0 = 0X00;
+		TIFR = 0X01;
+		timeroverflowcount++;
+		if(timeroverflowcount>=5)
+		{
+			PORTB ^=(0X01 << LED);
+			timeroverflowcount=0;
+		}
+	}
+}
+
